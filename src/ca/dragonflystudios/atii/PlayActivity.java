7feea1f0cc530109453cadce94bd5d10857a8f86
Jun Luo <jun@turix.ca>
2012-11-17 22:10:58 -0500

    start PlayActivity

---
+++ src/ca/dragonflystudios/atii/PlayActivity.java
@@ -0,0 +1,17 @@
+package ca.dragonflystudios.atii;
+
+import android.app.Activity;
+
+/*
+ * TODO:
+ * [ ] Playback a series of photos and paired audios.
+ * [ ] Playback a series of PDF pages and paird audios.
+ * [ ] Basic control for playback in place.
+ * [ ] Double tap to zoom in
+ * [ ] Page-based navigation
+ */
+
+public class PlayActivity extends Activity
+{
+
+}
