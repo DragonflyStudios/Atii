319447ba7304d65fc8212d91ab962e87a84e1722
Jun Luo <jun@turix.ca>
2012-11-19 09:27:31 -0500

    todo notes update

--- src/ca/dragonflystudios/atii/PlayActivity.java
+++ src/ca/dragonflystudios/atii/PlayActivity.java
@@ -4,11 +4,14 @@ import android.app.Activity;
 
 /*
  * TODO:
- * [ ] Playback a series of photos and paired audios.
- * [ ] Playback a series of PDF pages and paird audios.
- * [ ] Basic control for playback in place.
- * [ ] Double tap to zoom in
- * [ ] Page-based navigation
+ * [ ] Playback a series of photos 1-day
+ * [ ] Double tap to zoom in 0.5-day
+ * [ ] Page-based navigation 0.5-day
+ * [ ] Playback a series of PDF pages 1-day
+ * [ ] Playback audios 1-day
+ * [ ] Basic control for playback in place 1-day
+ * [ ] Replace audio with recording 0.5-day
+ * [ ] Replace page image with photo 0.5-day
  */
 
 public class PlayActivity extends Activity
